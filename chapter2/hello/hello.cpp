fbd86c5202811fc4afe58167b46b9c7b6f2542f1
hisatsune <hisatsune@m3l.co.jp>
2021-08-05 10:17:25 +0900

    First Commit

---
+++ chapter2/hello/hello.cpp
@@ -0,0 +1,8 @@
+#include "all.h"
+
+int main()
+{
+  std::cout << "Hello "s;
+  std::cout << "World!"s;
+  //std::cout << "\n"s;
+}
---
+++ chapter3/iostream/iotest.cpp
@@ -0,0 +1,9 @@
+#include "all.h"
+
+int main()
+{
+  std::cout
+  << "Integer : "s << 3 + 5 << " "s << 3 - 5 << " "s
+  << 3 * 5 << " "s << 3 / 5 << " "s << 3 % 5 << "\n"s
+  << "Floating Point : "s << 3.14 << "\n"s;
+}
